2c5a2ec991bfc2d5d69781b6091f018adc9be511
indeex <indeex@qq.com>
2018-07-08 23:51:39 +0800

    init

---
+++ precache-manifest.d88a3fc99cb0fdbaab5ffc97d72666e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "885e42c128e25c34be164668f3f8ac84",
+    "url": "/static/css/base.css"
+  },
+  {
+    "revision": "9c3ee51149f299aabf80",
+    "url": "/js/chunk-vendors.9c3ee511.js"
+  },
+  {
+    "revision": "51982f64d5743e382742",
+    "url": "/js/app.51982f64.js"
+  },
+  {
+    "revision": "0221e9dc49865b16d8082de9d188158d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51982f64d5743e382742",
+    "url": "/css/app.9596656c.css"
+  }
+];
